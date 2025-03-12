d71052bb62b88652f9ee92d6ff30b885c0001510
ErixTNT <venturinierie@gmail.com>
2025-03-12 00:34:25 -0300

    sin querer borre prinerpagina

--- app/Views/primerpagina.php
+++ app/Views/primerpagina.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-<link rel="manifest" href="/manifest.json">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-Skate: Revoluciona tu Movimiento</title>
@@ -279,7 +278,6 @@ footer a:hover {
 }
 </style>
     </style>
-    
 </head>
 <body>
 
