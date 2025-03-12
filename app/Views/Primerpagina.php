978b7264c9bb7fc2041e57a6060402ab3ec372d5
ErixTNT <venturinierie@gmail.com>
2025-03-12 13:22:05 -0300

    prueba

--- app/Views/Primerpagina.php
+++ app/Views/Primerpagina.php
@@ -4,7 +4,7 @@
 <link rel="manifest" href="/manifest.json">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-Skate: Revoluciona tu Movimiento</title>
+    <title>E-Skate: Revoluciona tu Movimient</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
