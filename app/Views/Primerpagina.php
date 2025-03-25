3e8458a5aec45d5f11b8df57c2fcc19068271909
ErixTNT <venturinierie@gmail.com>
2025-03-25 12:00:17 -0300

    NASHE

--- app/Views/Primerpagina.php
+++ app/Views/Primerpagina.php
@@ -285,6 +285,27 @@ footer a:hover {
         font-size: 0.9rem;
         padding: 8px 15px;
     }
+    #installButton {
+    display: none; /* Oculto por defecto */
+    background-color: #ffcc00;
+    color: #333;
+    padding: 15px 30px;
+    font-size: 1.2rem;
+    border-radius: 50px;
+    border: none;
+    transition: all 0.3s ease;
+    box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.3);
+    cursor: pointer;
+}
+
+#installButton:hover {
+    background-color: #ffb700;
+    transform: scale(1.05);
+}
+
+#installButton:active {
+    transform: scale(0.98);
+}
 }
 </style>
     </style>
