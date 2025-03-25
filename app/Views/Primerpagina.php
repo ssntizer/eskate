10923ce828f7177cb46ff43d01abd9f486f460c0
ErixTNT <venturinierie@gmail.com>
2025-03-25 12:33:30 -0300

    sigo intentando cambiar el estilo

--- app/Views/Primerpagina.php
+++ app/Views/Primerpagina.php
@@ -315,7 +315,7 @@ footer a:hover {
 <div class="header">
     <h1>E-Skate</h1>
     <div>
-    <button id="installButton" style="display:none;">Instalar PWA</button>
+    <button id="installButton" class="btn-install">Instalar PWA</button>
         <a href="#quienes-somos">¿Quiénes Somos?</a>
         <a href="#contactanos">Contáctanos</a>
         <a href="<?= site_url('login') ?>">Ingresar</a>
